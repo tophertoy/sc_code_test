953d698866d7c428d67251cfa5ae98a202c01fa3
Chris <ctoynbee@gmail.com>
2025-06-29 10:21:33 +1200

    Add indexes to client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_27_235642) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_28_222037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_27_235642) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_clients_on_email"
+    t.index ["full_name"], name: "index_clients_on_full_name"
   end
 end
